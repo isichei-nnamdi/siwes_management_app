980a29e28abb75fa020d303fc374e86042ec3ac8
Nnamdi Isichei <71382968+isichei-nnamdi@users.noreply.github.com>
2025-05-05 14:22:59 +0100

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from database.models import Base  # import your Base from the file where models are defined
 
-engine = create_engine('sqlite:///./data/siwes.db') 
+engine = create_engine('sqlite:///./database/data/siwes.db') 
 Base.metadata.create_all(engine)
