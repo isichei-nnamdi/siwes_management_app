83517d4e1a18bfb2e8658fc3920321f2343f9919
Nnamdi Isichei <71382968+isichei-nnamdi@users.noreply.github.com>
2025-05-05 14:21:31 +0100

    Update models.py

--- database/models.py
+++ database/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
 
 
-DATABASE_URL = "sqlite:///./data/database.db"
+DATABASE_URL = "sqlite:///./data/siwes.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
